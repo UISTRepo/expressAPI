a0c4a553bdd8af69ee09ff0a696b803c8280456b
Naum Tuntev <tuntev@gmail.com>
2021-05-17 09:45:48 +0200

    Removed the password

--- connections/mysql.js
+++ connections/mysql.js
@@ -2,9 +2,8 @@ var mysql = require('mysql');
 
 var config = {
     host: 'localhost',
-    port: '8889',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'tutorialsIP'
 }
 
